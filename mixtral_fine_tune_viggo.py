19134d1f8ca1d49591dc0cf9bf5973a2eab277e3
BigsnarfDude <bigsnarfdude@users.noreply.github.com>
2023-12-18 11:54:42 -0700

    Create mixtral_fine_tune_viggo.py

---
+++ mixtral_fine_tune_viggo.py
@@ -0,0 +1,195 @@
+from accelerate import FullyShardedDataParallelPlugin, Accelerator
+from torch.distributed.fsdp.fully_sharded_data_parallel import FullOptimStateDictConfig, FullStateDictConfig
+from datasets import load_dataset
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM, DataCollatorForLanguageModeling, BitsAndBytesConfig
+import matplotlib.pyplot as plt
+from peft import prepare_model_for_kbit_training
+from peft import LoraConfig, get_peft_model
+import transformers
+from datetime import datetime
+
+fsdp_plugin = FullyShardedDataParallelPlugin(
+    state_dict_config=FullStateDictConfig(offload_to_cpu=True, rank0_only=False),
+    optim_state_dict_config=FullOptimStateDictConfig(offload_to_cpu=True, rank0_only=False),
+)
+
+accelerator = Accelerator(fsdp_plugin=fsdp_plugin)
+train_dataset = load_dataset('gem/viggo', split='train')
+eval_dataset = load_dataset('gem/viggo', split='validation')
+test_dataset = load_dataset('gem/viggo', split='test')
+print(train_dataset)
+print(eval_dataset)
+print(test_dataset)
+
+base_model_id = "mistralai/Mixtral-8x7B-v0.1"
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_compute_dtype=torch.bfloat16
+)
+
+model = AutoModelForCausalLM.from_pretrained(base_model_id, quantization_config=bnb_config, device_map="auto")
+
+tokenizer = AutoTokenizer.from_pretrained(
+    base_model_id,
+    add_eos_token=True,
+    add_bos_token=True, 
+)
+
+def tokenize(prompt):
+    result = tokenizer(prompt)
+    result["labels"] = result["input_ids"].copy()
+    return result
+
+def generate_and_tokenize_prompt(data_point):
+    full_prompt =f"""Given a target sentence construct the underlying meaning representation of the input sentence as a single function with attributes and attribute values.
+This function should describe the target string accurately and the function must be one of the following ['inform', 'request', 'give_opinion', 'confirm', 'verify_attribute', 'suggest', 'request_explanation', 'recommend', 'request_attribute'].
+The attributes must be one of the following: ['name', 'exp_release_date', 'release_year', 'developer', 'esrb', 'rating', 'genres', 'player_perspective', 'has_multiplayer', 'platforms', 'available_on_steam', 'has_linux_release', 'has_mac_release', 'specifier']
+
+### Target sentence:
+{data_point["target"]}
+
+### Meaning representation:
+{data_point["meaning_representation"]}
+"""
+    return tokenize(full_prompt)
+
+tokenized_train_dataset = train_dataset.map(generate_and_tokenize_prompt)
+tokenized_val_dataset = eval_dataset.map(generate_and_tokenize_prompt)
+
+untokenized_text = tokenizer.decode(tokenized_train_dataset[1]['input_ids']) 
+print(untokenized_text)
+
+max_length = 320 
+tokenizer = AutoTokenizer.from_pretrained(
+    base_model_id,
+    padding_side="left",
+    add_eos_token=True,  
+    add_bos_token=True,  
+)
+tokenizer.pad_token = tokenizer.eos_token
+
+
+def tokenize(prompt):
+    result = tokenizer(
+        prompt,
+        truncation=True,
+        max_length=max_length,
+        padding="max_length",
+    )
+    result["labels"] = result["input_ids"].copy()
+    return result
+
+
+tokenized_train_dataset = train_dataset.map(generate_and_tokenize_prompt)
+tokenized_val_dataset = eval_dataset.map(generate_and_tokenize_prompt)
+print(tokenized_train_dataset[4]['input_ids'])
+untokenized_text = tokenizer.decode(tokenized_train_dataset[1]['input_ids']) 
+print(untokenized_text)
+
+print("Target Sentence: " + test_dataset[1]['target'])
+print("Meaning Representation: " + test_dataset[1]['meaning_representation'] + "\n")
+
+eval_prompt = """Given a target sentence construct the underlying meaning representation of the input sentence as a single function with attributes and attribute values.
+This function should describe the target string accurately and the function must be one of the following ['inform', 'request', 'give_opinion', 'confirm', 'verify_attribute', 'suggest', 'request_explanation', 'recommend', 'request_attribute'].
+The attributes must be one of the following: ['name', 'exp_release_date', 'release_year', 'developer', 'esrb', 'rating', 'genres', 'player_perspective', 'has_multiplayer', 'platforms', 'available_on_steam', 'has_linux_release', 'has_mac_release', 'specifier']
+
+### Target sentence:
+Earlier, you stated that you didn't have strong feelings about PlayStation's Little Big Adventure. Is your opinion true for all games which don't have multiplayer?
+
+### Meaning representation:
+"""
+
+model = accelerator.prepare_model(model)
+
+eval_tokenizer = AutoTokenizer.from_pretrained(
+    base_model_id,
+    add_bos_token=True,
+)
+
+device = "cuda"
+model_input = eval_tokenizer(eval_prompt, return_tensors="pt").to(device)
+
+model.eval()
+with torch.no_grad():
+    print(eval_tokenizer.decode(model.generate(**model_input, max_new_tokens=128)[0], skip_special_tokens=True))
+
+model.gradient_checkpointing_enable()
+model = prepare_model_for_kbit_training(model)
+
+def print_trainable_parameters(model):
+    """
+    Prints the number of trainable parameters in the model.
+    """
+    trainable_params = 0
+    all_param = 0
+    for _, param in model.named_parameters():
+        all_param += param.numel()
+        if param.requires_grad:
+            trainable_params += param.numel()
+    print(
+        f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param}"
+    )
+
+
+config = LoraConfig(
+    r=8,
+    lora_alpha=16,
+    target_modules=[
+        "q_proj",
+        "k_proj",
+        "v_proj",
+        "o_proj",
+        "w1",
+        "w2",
+        "w3",
+        "lm_head",
+    ],
+    bias="none",
+    lora_dropout=0.05,  # Conventional
+    task_type="CAUSAL_LM",
+)
+
+model = get_peft_model(model, config)
+print_trainable_parameters(model)
+
+model = accelerator.prepare_model(model)
+print(model)
+
+project = "viggo-finetune"
+base_model_name = "mixtral"
+run_name = base_model_name + "-" + project
+output_dir = "./" + run_name
+
+tokenizer.pad_token = tokenizer.eos_token
+
+trainer = transformers.Trainer(
+    model=model,
+    train_dataset=tokenized_train_dataset,
+    eval_dataset=tokenized_val_dataset,
+    args=transformers.TrainingArguments(
+        output_dir=output_dir,
+        warmup_steps=5,
+        per_device_train_batch_size=1,
+        gradient_checkpointing=True,
+        gradient_accumulation_steps=4,
+        max_steps=500,
+        learning_rate=2.5e-5, 
+        logging_steps=25,
+        fp16=True, 
+        optim="paged_adamw_8bit",
+        logging_dir="./logs",        # Directory for storing logs
+        save_strategy="steps",       # Save the model checkpoint every logging step
+        save_steps=50,                # Save checkpoints every 50 steps
+        evaluation_strategy="steps", # Evaluate the model every logging step
+        eval_steps=50,               # Evaluate and save checkpoints every 50 steps
+        do_eval=True,                # Perform evaluation at the end of training
+        report_to="wandb",           # Comment this out if you don't want to use weights & baises
+        run_name=f"{run_name}-{datetime.now().strftime('%Y-%m-%d-%H-%M')}"          # Name of the W&B run (optional)
+    ),
+    data_collator=transformers.DataCollatorForLanguageModeling(tokenizer, mlm=False),
+)
+
+model.config.use_cache = False  # silence the warnings. Please re-enable for inference!
+trainer.train()
